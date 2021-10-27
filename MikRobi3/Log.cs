771d82d1a9b32065c03e9419414bd26b5ff6d3e4
spurikutya <willisthewolfdog@gmail.com>
2021-10-27 15:33:55 +0200

    megcsináltam a verziókezelést, mert a VS szarrá szivatott vele

---
+++ MikRobi3/Log.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace MikRobi3
+{
+    class Log
+    {
+        const string logPath = "/var/log/MikRobi";
+        const string logErrorFilename = "error.log";
+        const string logSecurityFilename = "security.log";
+        const string logMiscFilename = "misc.log";
+
+        public Log()
+        {
+            ;
+        }
+    }
+}
--- MikRobi3/Program.cs
+++ MikRobi3/Program.cs
@@ -6,7 +6,7 @@ namespace MikRobi3
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine(System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
         }
     }
 }
