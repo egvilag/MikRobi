8e1a050a4a194310d19f7b0f48d256c7c7dfa199
spurikutya <willisthewolfdog@gmail.com>
2021-10-28 15:05:21 +0200

    Alapszintű logolás

--- MikRobi3/Log.cs
+++ MikRobi3/Log.cs
@@ -11,10 +11,147 @@ namespace MikRobi3
         const string logErrorFilename = "error.log";
         const string logSecurityFilename = "security.log";
         const string logMiscFilename = "misc.log";
+        const int maxLogsize = 600;
 
-        public Log()
+        StreamWriter swError;
+        StreamWriter swSecurity;
+        StreamWriter swMisc;
+
+        public void Open()
         {
-            ;
+            if (!Directory.Exists(logPath))
+            {
+                try
+                {
+                    Directory.CreateDirectory(logPath);
+                }
+                catch
+                {
+                    Console.WriteLine("Log path not found, failed to create directory: " + logPath);
+                    Environment.Exit(1);
+                }
+            }
+            if (!File.Exists(logErrorFilename))
+            {
+                try
+                {
+                    swError = new StreamWriter(logPath + "/" + logErrorFilename, true, Encoding.UTF8, 65535);
+                }
+                catch
+                {
+                    Console.WriteLine("Failed to open the " + logErrorFilename + " file for write.");
+                    Environment.Exit(1);
+                }
+            }
+            if (!File.Exists(logSecurityFilename))
+            {
+                try
+                {
+                    swSecurity = new StreamWriter(logPath + "/" + logSecurityFilename, true, Encoding.UTF8, 65535);
+                }
+                catch
+                {
+                    Console.WriteLine("Failed to open the " + logSecurityFilename + " file for write.");
+                    Environment.Exit(1);
+                }
+            }
+            if (!File.Exists(logMiscFilename))
+            {
+                try
+                {
+                    swMisc = new StreamWriter(logPath + "/" + logMiscFilename, true, Encoding.UTF8, 65535);
+                }
+                catch
+                {
+                    Console.WriteLine("Failed to open the " + logMiscFilename + " file for write.");
+                    Environment.Exit(1);
+                }
+            }
+        }
+
+        public void Close()
+        {
+            swError.Close();
+            swError = null;
+            swSecurity.Close();
+            swSecurity = null;
+            swMisc.Close();
+            swMisc = null;
+        }
+
+        public void Write(string file, string message)
+        {
+            switch (file)
+            {
+                case "error": swError.WriteLine(DateTime.Now.ToString() + "\t> " + message); swError.Flush(); break;
+                case "security": swSecurity.WriteLine(DateTime.Now.ToString() + "\t> " + message); swSecurity.Flush(); break;
+                case "misc": swMisc.WriteLine(DateTime.Now.ToString() + "\t> " + message); swMisc.Flush(); break;
+            }
+            CheckFileSize(file);
+        }
+
+        private void CheckFileSize(string file)
+        {
+            switch (file)
+            {
+                case "error":
+                    if (new FileInfo(logPath + "/" + logErrorFilename).Length > maxLogsize)
+                    {
+                        swError.Close();
+                        using (System.Diagnostics.Process pProcess = new System.Diagnostics.Process())
+                        {
+                            pProcess.StartInfo.FileName = "gzip";
+                            pProcess.StartInfo.Arguments = logPath + "/" + logErrorFilename; //argument
+                            pProcess.StartInfo.UseShellExecute = false;
+                            pProcess.StartInfo.RedirectStandardOutput = true;
+                            pProcess.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                            pProcess.StartInfo.CreateNoWindow = true; //not diplay a windows
+                            pProcess.Start();
+                            string output = pProcess.StandardOutput.ReadToEnd(); //The output result
+                            pProcess.WaitForExit();
+                        }
+                        swError = new StreamWriter(logPath + "/" + logErrorFilename, true, Encoding.UTF8, 65535);
+                    }
+                    break;
+                case "security":
+                    if (new FileInfo(logPath + "/" + logSecurityFilename).Length > maxLogsize)
+                    {
+                        swSecurity.Close();
+                        using (System.Diagnostics.Process pProcess = new System.Diagnostics.Process())
+                        {
+                            pProcess.StartInfo.FileName = "gzip";
+                            pProcess.StartInfo.Arguments = logPath + "/" + logSecurityFilename; //argument
+                            pProcess.StartInfo.UseShellExecute = false;
+                            pProcess.StartInfo.RedirectStandardOutput = true;
+                            pProcess.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                            pProcess.StartInfo.CreateNoWindow = true; //not diplay a windows
+                            pProcess.Start();
+                            string output = pProcess.StandardOutput.ReadToEnd(); //The output result
+                            pProcess.WaitForExit();
+                        }
+                        swSecurity = new StreamWriter(logPath + "/" + logSecurityFilename, true, Encoding.UTF8, 65535);
+                    }
+                    break;
+                case "misc": 
+                    if (new FileInfo(logPath + "/" + logMiscFilename).Length > maxLogsize)
+                    {
+                        swMisc.Close();
+                        using (System.Diagnostics.Process pProcess = new System.Diagnostics.Process())
+                        {
+                            pProcess.StartInfo.FileName = "gzip";
+                            pProcess.StartInfo.Arguments = logPath + "/" + logMiscFilename; //argument
+                            pProcess.StartInfo.UseShellExecute = false;
+                            pProcess.StartInfo.RedirectStandardOutput = true;
+                            pProcess.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                            pProcess.StartInfo.CreateNoWindow = true; //not diplay a windows
+                            pProcess.Start();
+                            string output = pProcess.StandardOutput.ReadToEnd(); //The output result
+                            pProcess.WaitForExit();
+                        }
+                        swMisc = new StreamWriter(logPath + "/" + logMiscFilename, true, Encoding.UTF8, 65535);
+                    }
+                    break;
+            }
         }
     }
 }
--- MikRobi3/Program.cs
+++ MikRobi3/Program.cs
@@ -4,9 +4,15 @@ namespace MikRobi3
 {
     class Program
     {
+        public static Log log;
+
         static void Main(string[] args)
         {
-            Console.WriteLine(System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
+            Console.WriteLine("MikRobi v" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
+            log = new Log();
+            log.Open();
+            log.Write("misc", "Program started.");
+            log.Close();
         }
     }
 }
